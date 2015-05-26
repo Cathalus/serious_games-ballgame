3c386a1e276210f72c57868239844d7a2a371907
Raymond Walter <raymond.walter@uni-ulm.de>
2015-05-26 16:15:36 +0200

    Wind fix

---
+++ Assets/Scripts/LevelDynamics/Platform_Wind.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Platform that emits Wind and applies force to the player
+/// </summary>
+public class Platform_Wind : MonoBehaviour {
+
+    public float WindStrength = 5.0f;
+
+	void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag(Tags.Player))
+        {
+            // Do Stuff
+            Rigidbody playerBody = other.GetComponent<Rigidbody>();
+            playerBody.AddForce(transform.up*WindStrength*10*Time.deltaTime, ForceMode.Force);
+        }
+    }
+
+}
