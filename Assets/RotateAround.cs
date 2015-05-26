0c54f252a48b5ebded8a9cbf6b31ba4e7d233ac1
Raymond Walter <raymond.walter@uni-ulm.de>
2015-05-26 15:41:03 +0200

    Basic Main Menu
    Implementation of a basic main menu with level selection

---
+++ Assets/RotateAround.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+
+    public GameObject RotateTarget;
+    public float RotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(RotateTarget.transform);
+        transform.Translate(Vector3.right * Time.deltaTime * RotateSpeed);
+	}
+}
---
+++ Assets/Scripts/GUI/StartLevel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartLevel : MonoBehaviour {
+
+    public int Level = -1;
+
+    private Button _btn;
+
+    void Awake()
+    {
+        _btn = GetComponent<Button>();
+        _btn.onClick.AddListener(() => { ButtonClicked(); });
+    }
+    
+    void ButtonClicked()
+    {
+        if(Level != -1)
+        {
+            Application.LoadLevel(Level);
+        }
+    }
+	
+}
