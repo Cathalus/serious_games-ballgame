081452e35e022f5d3fd22051a2c6b32f96f326cc
Raymond Walter <raymond.walter@uni-ulm.de>
2015-05-27 17:40:08 +0200

    Exit Game Button

---
+++ Assets/Scripts/GUI/ExitGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ExitGame : MonoBehaviour {
+
+    private Button _btn;
+
+    void Awake()
+    {
+        _btn = GetComponent<Button>();
+        _btn.onClick.AddListener(() => { ButtonClicked(); });
+    }
+
+    void ButtonClicked()
+    {
+        Application.Quit();
+    }
+}
