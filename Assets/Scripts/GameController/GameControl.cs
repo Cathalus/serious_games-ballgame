890c970432879e99831d8ff31631af67d99cde7b
Raymond Walter <raymond.walter@uni-ulm.de>
2015-05-27 16:24:52 +0200

    Addded Background Music, Level Reset Button

---
+++ Assets/Scripts/GameController/GameControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+
+	void Update () {
+	    
+        // Press Start to go to main menu
+        if(Input.GetButtonDown("Start"))
+        {
+            Application.LoadLevel(0);
+            // Press Y to reset the current level
+        }else if(Input.GetButtonDown("Reset"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+	}
+}
